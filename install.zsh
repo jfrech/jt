921ca1b55cb2fece9827545a5a28c2c6c078b304
Jonathan Frech <info@jfrech.com>
2020-05-29 22:38:58 +0200

    Fix.

--- install.zsh
+++ install.zsh
@@ -3,7 +3,6 @@
 jt_repository="https://raw.githubusercontent.com/jfrech/jt/master"
 jt_local="$HOME/.jt"
 
-
 [ -d "$jt_local" ] \
     && echo "Cannot install jt; '$jt_local' already exists." && exit 1
 
@@ -12,12 +11,15 @@ require () {
 require curl
 require sed
 
+curl_fail () {
+    echo "Could not curl '$jt_repository/jt'." \
+        && rm "$jt_local/jt" && rmdir "$jt_local" && exit 1 }
+
 mkdir -p "$jt_local"
 curl --silent --fail "$jt_repository/jt" \
     | sed "s|^jt_repository=\"[^\"]*\"\$|jt_repository=\"$jt_repository\"|" \
     > "$jt_local/jt" \
-    || echo "Could not curl '$jt_repository/jt'." \
-    && rm "$jt_local/jt" && rmdir "$jt_local" && exit 1
+    || curl_fail
 
 chmod +x "$jt_local/jt"
 echo "Please add '$jt_local' to your PATH."
