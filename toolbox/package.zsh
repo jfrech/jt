e04f5758a40531f1a6ad884c0f9cde9f6afc83de
Jonathan Frech <info@jfrech.com>
2020-05-30 01:59:27 +0200

    Tool fix.

--- toolbox/package.zsh
+++ toolbox/package.zsh
@@ -4,7 +4,7 @@ jt_local_toolbox="$(realpath "$(dirname "$0")")"
 cd "$jt_local_toolbox"
 
 find . | grep '^\./\([^./]*\)/Makefile$' \
-    | sed 's|^\./\([^./]\)/Makefile$|./\1/\1|' | xargs -l rm -f
+    | sed 's|^\./\([^./]*\)/Makefile$|./\1/\1|' | xargs -l rm -f
 find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep '\.tar\.gz$' \
     | xargs -l rm -f
 find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep -v '\.' \
