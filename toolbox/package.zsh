f0e15465997f9833cfe397e023c9632419e024e8
Jonathan Frech <info@jfrech.com>
2020-05-29 23:46:03 +0200

    Refactoring.

--- toolbox/package.zsh
+++ toolbox/package.zsh
@@ -1,3 +1,11 @@
-find . | grep '^\./\([^./]*\)/Makefile$' | sed 's|^\./\([^./]\)/Makefile$|./\1/\1|' | xargs -l rm -f
-find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep '\.tar\.gz$' | xargs -l rm -f
-find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep -v '\.' | sed 's|^\(.*\)$|\1.tar.gz \1|' | xargs -l tar -czf
+#! /bin/zsh
+
+jt_local_toolbox="$(realpath "$(dirname "$0")")"
+cd "$jt_local_toolbox"
+
+find . | grep '^\./\([^./]*\)/Makefile$' \
+    | sed 's|^\./\([^./]\)/Makefile$|./\1/\1|' | xargs -l rm -f
+find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep '\.tar\.gz$' \
+    | xargs -l rm -f
+find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep -v '\.' \
+    | sed 's|^\(.*\)$|\1.tar.gz \1|' | xargs -l tar -czf
