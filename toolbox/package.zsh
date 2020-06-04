06c675e7fd00f6559ebba361b2c9ba277285f5e0
jfrech <info@jfrech.com>
2020-06-04 22:36:33 +0200

    Trying to reduce tarball variability.

--- toolbox/package.zsh
+++ toolbox/package.zsh
@@ -8,7 +8,8 @@ find . | grep '^\./\([^./]*\)/Makefile$' \
 find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep '\.tar\.gz$' \
     | xargs -l rm -f
 find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep -v '\.' \
-    | sed 's|^\(.*\)$|\1.tar \1|' | xargs -l tar -cf
+    | sed 's|^\(.*\)$|\1.tar \1|' | xargs -l \
+    tar --sort=name -cf
 find . -maxdepth 1 | grep '^\./[a-zA-Z-]\+\.tar$' | xargs -l \
     gzip --no-name --best
 
