58b980641551bd1eb9de6ff3c4b4e31f4f9a8fd4
jfrech <info@jfrech.com>
2020-06-04 22:29:27 +0200

    Packaging change.

--- toolbox/package.zsh
+++ toolbox/package.zsh
@@ -9,7 +9,8 @@ find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep '\.tar\.gz$' \
     | xargs -l rm -f
 find . -maxdepth 1 | grep '^\./' | sed 's|^\./||' | grep -v '\.' \
     | sed 's|^\(.*\)$|\1.tar \1|' | xargs -l tar -cf
-find . -maxdepth 1 | grep '^\./[a-zA-Z-]\+\.tar$' | xargs -l gzip --best
+find . -maxdepth 1 | grep '^\./[a-zA-Z-]\+\.tar$' | xargs -l \
+    gzip --no-name --best
 
 find . -mindepth 2 | grep -o '^\./[^./]*/' | sed 's|^\./||; s|/$||' \
     | uniq | sort > tools.txt
