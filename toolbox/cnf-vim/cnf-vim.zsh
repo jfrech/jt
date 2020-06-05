18b2d647fd8388d992fb2162c50c27f9f5c73421
jfrech <info@jfrech.com>
2020-06-05 14:23:18 +0200

    cnf-vim fix.

--- toolbox/cnf-vim/cnf-vim.zsh
+++ toolbox/cnf-vim/cnf-vim.zsh
@@ -1,13 +1,12 @@
 #! /bin/zsh
 
 cd "$(dirname "$0")"
-
-# 'make' will already have run on install
-make > /dev/null
+newlines_file = "$(realpath "$(dirname "$0")/newlines.txt")"
 
 colorscheme_shell="$(./create_colorscheme "${@:1}")"
 
-< vimrc | eval "$colorscheme_shell" > "$HOME/.vimrc"
+< vimrc | eval "$colorscheme_shell" \
+    | sed "s|%newlines_file|$newlines_file|g" > "$HOME/.vimrc"
 
 mkdir -p "$HOME/.vim/colors"
 < j.vim.pre | eval "$colorscheme_shell" > "$HOME/.vim/colors/j.vim"
