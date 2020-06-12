469d3b99e974450f5743721685af5d21687d6d20
Jonathan Frech <info@jfrech.com>
2020-06-12 20:19:04 +0200

    Dollar fix.

--- toolbox/cnf-vim/cnf-vim.zsh
+++ toolbox/cnf-vim/cnf-vim.zsh
@@ -8,10 +8,10 @@ jt="$dot_jt/jt"
 newlines_file="$tool_dir/newlines.txt"
 colorscheme_shell="$("$tool_dir/create_colorscheme" "${@:1}")"
 
-"jt" safe rmf "$HOME/.vimrc"
+"$jt" safe rmf "$HOME/.vimrc"
 < "$tool_dir/vimrc" | eval "$colorscheme_shell" \
     | sed "s|%newlines_file|$newlines_file|g" > "$HOME/.vimrc"
 
 mkdir -p "$HOME/.vim/colors"
-"jt" safe rmf "$HOME/.vim/colors/j.vim"
+"$jt" safe rmf "$HOME/.vim/colors/j.vim"
 < "$tool_dir/j.vim.pre" | eval "$colorscheme_shell" > "$HOME/.vim/colors/j.vim"
