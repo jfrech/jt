dd9f0de430e7cd0f22bbf03ddf7251db95e98361
Jonathan Frech <info@jfrech.com>
2020-06-12 19:49:13 +0200

    Fix.

--- toolbox/cnf-vim/cnf-vim.zsh
+++ toolbox/cnf-vim/cnf-vim.zsh
@@ -9,9 +9,9 @@ newlines_file="$tool_dir/newlines.txt"
 colorscheme_shell="$("$tool_dir/create_colorscheme" "${@:1}")"
 
 "jt" safe rmf "$HOME/.vimrc"
-< vimrc | eval "$colorscheme_shell" \
+< "$tool_dir/vimrc" | eval "$colorscheme_shell" \
     | sed "s|%newlines_file|$newlines_file|g" > "$HOME/.vimrc"
 
 mkdir -p "$HOME/.vim/colors"
 "jt" safe rmf "$HOME/.vim/colors/j.vim"
-< j.vim.pre | eval "$colorscheme_shell" > "$HOME/.vim/colors/j.vim"
+< "$tool_dir/j.vim.pre" | eval "$colorscheme_shell" > "$HOME/.vim/colors/j.vim"
