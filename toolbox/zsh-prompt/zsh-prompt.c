172d241115d6ee434b02b7c9de790ea8177a5523
Jonathan Frech <info@jfrech.com>
2020-11-04 21:34:35 +0100

    `zsh-prompt --cloud`

--- toolbox/zsh-prompt/zsh-prompt.c
+++ toolbox/zsh-prompt/zsh-prompt.c
@@ -1,8 +1,9 @@
 /* Jonathan Frech, 2020-04-08, 2020-04-17, 2020-04-18, 2020-11-04 */
 /* Builds a zsh prompt. */
 
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 const int rainbow[] = { 196, 202, 208, 214, 220, 226, 190, 154, 118, 82, 46, 47,
@@ -10,13 +11,19 @@ const int rainbow[] = { 196, 202, 208, 214, 220, 226, 190, 154, 118, 82, 46, 47,
     197, 196 };
 const size_t rainbow_n = sizeof (rainbow) / sizeof (int);
 
-int main() {
+bool streq(char *s, char *z) {
+    return *s ? *s == *z && streq(++s, ++z) : !*z ; }
+
+int main(int argc, char **argv) {
     time_t _time = time(NULL);
     struct tm *time = localtime(&_time);
 
     const char *now_fmt = "%H:%M:%S";
     char now[8 + 1];
     strftime(now, sizeof (now), now_fmt, time);
+
+    if (argc > 1 && streq("--cloud", argv[1]))
+        printf("%%{\33[38;5;%dm%%}\xf0\x9f\x8c\xa9 %%{\33[m%%} ", 18);
 
     size_t rainbow_shift = time->tm_sec;
     printf("%%{\33[1m%%}");
